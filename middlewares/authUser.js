853de5a84ca022676ccdf197286abfb1ec819221
Yash Kalamkar <yashkalamkar07@gmail.com>
2025-11-29 10:27:43 +0530

    first commit

---
+++ middlewares/authUser.js
@@ -0,0 +1,74 @@
+// import jwt from "jsonwebtoken";
+
+// const authUser = async (req, res, next) => {
+//     try {
+
+//         const token = req.cookies.token;
+//         if (!token) {
+//             return res.status(401).json({
+//                 message: "User not authorized",
+//                 success: false,
+//             })
+//         }
+
+//         const decode = await jwt.verify(token, process.env.SECRET_KEY);
+
+//         if (!decode) {
+//             return res.status(401).json({
+//                 message: "Invalid token",
+//                 success: false,
+//             })
+//         }
+
+//         req.id = decode.userId;
+
+//         next();
+
+
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+
+// export default authUser;
+
+
+
+
+import jwt from "jsonwebtoken";
+
+const authUser = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) {
+            return res.status(401).json({
+                message: "User not authorized",
+                success: false,
+            });
+        }
+
+        // Use correct env variable name
+        const decode = await jwt.verify(token, process.env.JWT_SECRET);
+
+        if (!decode) {
+            return res.status(401).json({
+                message: "Invalid token",
+                success: false,
+            });
+        }
+
+        req.id = decode.userId;
+        next();
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: "Something went wrong",
+            success: false,
+        });
+    }
+}
+
+export default authUser;
